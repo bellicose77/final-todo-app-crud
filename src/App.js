02b86bd7b0976e20fc59583be3c0a5e3a1181290
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-04 19:04:53 +0600

    task route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Task from "./components/Task/Task";
 
 
 function App() {
@@ -7,6 +9,11 @@ function App() {
     <div >
       <BrowserRouter>
       <Header/>
+      <Routes>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/task" element={<Task/>}/>
+      </Routes>
+     
       </BrowserRouter>
     
    
