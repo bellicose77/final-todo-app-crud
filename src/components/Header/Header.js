8ed0ef605009ae685730b4a98fd25ac3f8986bbf
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-04 18:46:42 +0600

    nav bar done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,9 +8,9 @@ const Header = () => {
     <Container>
     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
     <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#home">Task</Nav.Link>
+      <Nav.Link href="#features">Show task</Nav.Link>
+      <Nav.Link href="#pricing">Info</Nav.Link>
     </Nav>
     </Container>
   </Navbar>
