5477b8ad13e7115e3ef69e8b2d69d164d8f65045
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-05 23:37:23 +0600

    password

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -41,7 +41,7 @@ const Task = () => {
 
     <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
+    <Form.Control type="password" onChange={handlePassword} placeholder="Password" />
     </Form.Group>
   <Button variant="primary" type="submit">
     Submit
