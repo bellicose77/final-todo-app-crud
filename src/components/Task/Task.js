763b5a4ecd22584a56e617622daaaca9b4860e15
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-05 23:23:55 +0600

    handle name

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -25,7 +25,7 @@ const Task = () => {
    </Form.Group>
     <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Control type="email" onChange={handleEmail} placeholder="Enter email" />
    </Form.Group>
 
     <Form.Group className="mb-3" controlId="formBasicPassword">
