1ae173b92303947a1fe8eaf3356f431d7bad7ff9
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-05 23:18:04 +0600

    state hook

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
 const Task = () => {
+     const [user,setName]=useState("")
     const handleSubmit = e =>{
         e.preventDefault();
         console.log("submit button");
@@ -13,16 +14,16 @@ const Task = () => {
     <Form.Group className="mb-3">
     <Form.Label>Name</Form.Label>
     <Form.Control type="text" placeholder="Enter name" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
+   </Form.Group>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
     <Form.Control type="email" placeholder="Enter email" />
-  </Form.Group>
+   </Form.Group>
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
     <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
+    </Form.Group>
   <Button variant="primary" type="submit">
     Submit
   </Button>
