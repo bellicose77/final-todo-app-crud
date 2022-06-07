301b601dde8466ba7a438cc829d1877785e9187a
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-07 22:31:05 +0600

    data showing done

--- src/components/DataShow/DataShow.js
+++ src/components/DataShow/DataShow.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const DataShow = ({name,password,email}) => {
+const DataShow = (props) => {
+    const {name,email,password} = props.task;
     return (
         <>
             <tr>
-                <td>name</td>
+                <td>{name}</td>
                 <td>{email}</td>
                 <td>{password}</td>
+                <td><Button>X</Button><Button>Update</Button></td>
             </tr>
             
         </>
