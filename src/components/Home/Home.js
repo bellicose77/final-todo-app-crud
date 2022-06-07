619e8cb123e7ad5e21be86750a9aaaba3638e8b9
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-07 21:57:04 +0600

    interpolation data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     useEffect(()=>{
         fetch('http://localhost:5000/tasks')
         .then(res=>res.json())
-        .then(data=>console.log(data))
+        .then(data=>setTasks(data))
     },[])
 
 
@@ -15,30 +15,18 @@ const Home = () => {
         <Table striped bordered hover variant="dark">
   <thead>
     <tr>
-      <th>#</th>
+     
       <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
+      <th>Email </th>
+      <th>Password</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td colSpan={2}>Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+      {
+           tasks.map(task=><tr>{task.name}</tr>)
+      }
+   
+    
   </tbody>
 </Table>
         </div>
