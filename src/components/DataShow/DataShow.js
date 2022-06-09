275cd1272e9d356e198ae0685a476f6f851d8ccd
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-09 21:39:22 +0600

    send request

--- src/components/DataShow/DataShow.js
+++ src/components/DataShow/DataShow.js
@@ -5,6 +5,13 @@ const DataShow = (props) => {
     const {_id,name,email,password} = props.task;
     const handleDelete=(id)=>{
         console.log(id);
+        fetch(`http://localhost:5000/tasks/${id}`,{
+            method:'DELETE',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: 'Stringify.json()'
+        })
     }
     return (
         <>
