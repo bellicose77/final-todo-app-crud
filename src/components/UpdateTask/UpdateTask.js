f86d12512f72cd7b369d6a028784f8ae1d6d44bc
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-10 14:11:50 +0600

    set onchage

--- src/components/UpdateTask/UpdateTask.js
+++ src/components/UpdateTask/UpdateTask.js
@@ -23,12 +23,12 @@ const UpdateTask = () => {
    </Form.Group>
     <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" value={singletask.email} placeholder="Enter email" />
+    <Form.Control type="email" onChange={handleEmail} value={singletask.email} placeholder="Enter email" />
    </Form.Group>
 
     <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control type="password" value={singletask.password} placeholder="Password" />
+    <Form.Control type="password" onChange={handlePassword} value={singletask.password} placeholder="Password" />
     </Form.Group>
     <Button variant="primary" type="submit">
     Submit
