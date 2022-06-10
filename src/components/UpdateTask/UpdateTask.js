2cbe646c96b8ed028c95c2187d7c4e667ed217ee
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-10 11:59:09 +0600

    form done

--- src/components/UpdateTask/UpdateTask.js
+++ src/components/UpdateTask/UpdateTask.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import {useParams} from 'react-router-dom';
 const UpdateTask = () => {
     const {id} = useParams();
@@ -9,16 +9,16 @@ const UpdateTask = () => {
     <Form >
     <Form.Group className="mb-3">
     <Form.Label>Name</Form.Label>
-    <Form.Control type="text" onChange={handleName} placeholder="Enter name" />
+    <Form.Control type="text" placeholder="Enter name" />
    </Form.Group>
     <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" onChange={handleEmail} placeholder="Enter email" />
+    <Form.Control type="email" placeholder="Enter email" />
    </Form.Group>
 
     <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control type="password" onChange={handlePassword} placeholder="Password" />
+    <Form.Control type="password" placeholder="Password" />
     </Form.Group>
     <Button variant="primary" type="submit">
     Submit
