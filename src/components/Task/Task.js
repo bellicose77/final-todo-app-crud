7425fe2b052ed7c093833a7e13a8833b24b300f9
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-12 20:59:32 +0600

    navigate

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Task = () => {
      const [user,setUser]=useState("");
      const [email,setEmail] = useState("");
      const [password,setPassword] = useState("");
+     const navigate = useNavigate()
 
      const handleName = e =>{
          const Name = e.target.value;
